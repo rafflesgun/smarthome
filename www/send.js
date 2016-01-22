6f7fe4113cf413cfe064ca5deecc57eccf024e0d
Raffles Gun <rafflesgun@gmail.com>
2016-01-22 12:48:59 +0800

    update manual node call for lively.

--- www/send.js
+++ www/send.js
@@ -7,19 +7,19 @@ var libwiringPi = ffi.Library('/usr/local/lib/libwiringPi', {
                               'delayMicroseconds' :  [ 'void', ['int', 'int'] ]
                               })
 
-var libLivolo = ffi.Library('./libLivoloWrapper', {
+var libLivolo = ffi.Library('./node_modules/pi-livolo/libLivoloWrapper', {
                               'newLivolo' : ['pointer', ['char'] ],
                               'Livolo_SendButton': ['void', [ 'pointer', 'int', 'char' ] ]
                               })
 
-if (process.argv.length < 4) {
+if (process.argv.length < 5) {
     console.log('Arguments: Livolo Switch Code')
     process.exit()
 }
 
-var PIN = 1;
-var remoteId = parseInt(process.argv[2]);
-var keyCode = parseInt(process.argv[3]);
+var PIN = parseInt(process.argv[2]);
+var remoteId = parseInt(process.argv[3]);
+var keyCode = parseInt(process.argv[4]);
 var mySwitch = libLivolo.newLivolo(PIN);
 
 if (libwiringPi.wiringPiSetup() == -1){
